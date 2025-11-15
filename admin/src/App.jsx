739ca39631e50449340cbca05dcce2628960af1b
Arpit <arpit.utkarsh49@gmail.com>
2025-11-15 20:23:28 +0530

    n

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./Context/authContext.jsx";
 import Login from "./Pages/Login.jsx";
--- admin/src/ProtectedRoute.jsx
+++ admin/src/ProtectedRoute.jsx
@@ -1,4 +1,5 @@
 // src/Components/ProtectedRoute.jsx
+import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "./Context/authContext.jsx";
 
